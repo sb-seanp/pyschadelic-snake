3a282c53b4cca651758da9877f94e8c3d7cd9630
Sean Prasertsit <sean@prasertsit.com>
2014-12-26 14:54:28 -0800

    Changes to gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,40 @@
 var gulp        = require('gulp'),
-    server      = require('express'),
+    express      = require('express'),
+    app         = express();
+    concat      = require('gulp-concat'),
     jade        = require('gulp-jade'),
     stylus      = require('gulp-stylus'),
-    livereload  = require('livereload');
+    jshint      = require('gulp-jshint'),
+    stylish     = require('jshint-reporter'),
+    livereload  = require('gulp-livereload'),
+    path        = require('path');
+
+// Lint javascript
+gulp.task('lint', function(){
+    gulp.src('public/javascripts/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+// Compile Stylus stylesheets
+gulp.task('stylus', function(){
+    gulp.src('public/stylesheets/*.styl')
+        .pipe(stylus({
+            compress: true
+        }))
+        .pipe(gulp.dest('public/stylesheets/'))
+});
+
+// Compile Jade templates
+gulp.task('jade', function(){
+    gulp.src('views/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./views/'))
+});
+
+gulp.task('express', function(){
+    app.use(express.static(path.resolve('./bin')));
+    app.listen(3000);
+})
