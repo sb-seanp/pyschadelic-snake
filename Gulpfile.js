a63d53463809eb448e1a3e6ce9c99db83f76120d
Sean Prasertsit <sean@prasertsit.com>
2014-12-27 01:34:19 -0800

    Navbar now fixed, began work on background

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,30 @@
 var gulp        = require('gulp'),
-    express      = require('express'),
-    app         = express();
+    server      = require('gulp-express'),
     concat      = require('gulp-concat'),
     jade        = require('gulp-jade'),
     stylus      = require('gulp-stylus'),
+    nib         = require('nib'),
     jshint      = require('gulp-jshint'),
-    stylish     = require('jshint-reporter'),
+    stylish     = require('jshint-stylish'),
     livereload  = require('gulp-livereload'),
     path        = require('path');
 
 // Lint javascript
-gulp.task('lint', function(){
+/*gulp.task('lint', function(){
     gulp.src('public/javascripts/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
-});
+});*/
 
 // Compile Stylus stylesheets
 gulp.task('stylus', function(){
     gulp.src('public/stylesheets/*.styl')
         .pipe(stylus({
+            use: [nib()],
             compress: true
         }))
         .pipe(gulp.dest('public/stylesheets/'))
+        .pipe(livereload());
 });
 
 // Compile Jade templates
@@ -32,9 +34,19 @@ gulp.task('jade', function(){
             pretty: true
         }))
         .pipe(gulp.dest('./views/'))
+        .pipe(livereload());
 });
 
-gulp.task('express', function(){
-    app.use(express.static(path.resolve('./bin')));
-    app.listen(3000);
-})
+// Watch for changes and restart server if necessary
+gulp.task('server', function(){
+    // Start server
+    server.run({
+        file: 'bin/www'
+    });
+    livereload.listen();
+    gulp.watch('public/stylesheets/*.styl', ['stylus']);
+    gulp.watch('views/*.jade', ['jade']);
+});
+
+// Default task
+gulp.task('default', [/*'lint',*/ 'stylus', 'jade', 'server']);
